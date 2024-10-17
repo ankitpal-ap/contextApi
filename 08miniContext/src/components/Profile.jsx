128c7add4d2909ed24b3d64ebb4b1a2c9d8007c2
ankitpal-ap <a4ankitpal@gmail.com>
2024-10-17 12:40:44 +0530

    profile me UserContext me jo {}brakit tha us ko hataya

--- 08miniContext/src/components/Profile.jsx
+++ 08miniContext/src/components/Profile.jsx
@@ -1,15 +1,3 @@
-// import React,{useContext} from 'react';
-// import { UserContext } from '../context/UserContext'
-
-// function Profile() {
-//     const {user} = useContext(UserContext)
-  
-//   if (!user) return <div>please login</div>
-//   return <div>Welcome{user.Username}</div>
-// }
-
-// export default Profile
-
 import React, { useContext } from 'react';
 import UserContext from '../context/UserContext';
 function Profile() {
